194de27af8f897c7879e441238e5f1a2cc5278b5
System Administrator <root@NICHOLASs-MacBook-Air-2.local>
2019-04-01 14:12:42 -0700

    Added Data Classes

--- app/src/main/java/com/nocholla/kotlin/flight/tickets/view/MainActivity.kt
+++ app/src/main/java/com/nocholla/kotlin/flight/tickets/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.nocholla.kotlin.flight.tickets
+package com.nocholla.kotlin.flight.tickets.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.nocholla.kotlin.flight.tickets.R
 
 class MainActivity : AppCompatActivity() {
 
